72cc1273fa9fd78401bc8e1f515f42cb16632bfd
Paul Tiarks <ptiarks@salesforce.com>
2020-07-30 16:22:16 -0500

    Create project around scanner plugin in iOS directory

---
+++ platforms/ios/BarcodeScanner/BarcodeScanner/BarcodeScanner.h
@@ -0,0 +1,19 @@
+//
+//  BarcodeScanner.h
+//  BarcodeScanner
+//
+//  Created by Paul Tiarks on 7/30/20.
+//  Copyright © 2020 Salesforce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BarcodeScanner.
+FOUNDATION_EXPORT double BarcodeScannerVersionNumber;
+
+//! Project version string for BarcodeScanner.
+FOUNDATION_EXPORT const unsigned char BarcodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarcodeScanner/PublicHeader.h>
+
+
